0d64a73445e4deb67519b1daf505d21851b13131
RBTS <rbadmin@rabbitboots.com>
2024-02-23 20:13:57 -0400

    fix crash on right-click context menu

--- lib/prod_ui/shared/lgc_input_s.lua
+++ lib/prod_ui/shared/lgc_input_s.lua
@@ -166,7 +166,8 @@ function lgcInputS.mousePressLogic(self, button, mouse_x, mouse_y)
 
 		--print("text_box, current thimble", self.context.current_thimble, root.banked_thimble)
 
-		local pop_up = commonWimp.makePopUpMenu(self, self.pop_up_def, x, y)
+		local ax, ay = self:getAbsolutePosition()
+		local pop_up = commonWimp.makePopUpMenu(self, self.pop_up_def, ax + mouse_x, ay + mouse_y)
 		root:runStatement("rootCall_doctorCurrentPressed", self, pop_up, "menu-drag")
 
 		pop_up:tryTakeThimble()
