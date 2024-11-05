6d428989738c3e3c8515c265db8528c7d8d43127
RBTS <rbadmin@rabbitboots.com>
2024-11-05 13:47:33 -0400

    PropertiesBox / TreeBox work

--- demo_wimp.lua
+++ demo_wimp.lua
@@ -282,7 +282,7 @@ do
 		end
 		--]]
 		do
-			local frame = fr_demo_select:launch("plan_number_box")
+			local frame = fr_demo_select:launch("plan_properties_box")
 			frame.x = 64
 			frame.y = 64
 		end
