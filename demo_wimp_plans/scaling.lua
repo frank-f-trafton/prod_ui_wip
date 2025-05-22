601ac6869258e8c06823dbdaa1529639f9157e8f
RBTS <frank_f_trafton@yahoo.com>
2025-05-22 18:46:24 -0300

    WIMP Demo: clean up of 'scaling' page

--- demo_wimp_plans/scaling.lua
+++ demo_wimp_plans/scaling.lua
@@ -4,6 +4,54 @@ local plan = {}
 local demoShared = require("demo_shared")
 
 
+local function _updateScale(self)
+	local context = self.context
+	local btn = self.parent:findTag("btn_crt")
+	if not btn then
+		return
+	end
+	local old_scale, old_dpi = context:getScale(), context:getDPI()
+	local scale, dpi
+
+	local in_scale = self.parent:findTag("in_scale")
+	if in_scale then
+		scale = tonumber(in_scale:getText())
+	end
+	local in_dpi = self.parent:findTag("in_dpi")
+	if in_dpi then
+		dpi = tonumber(in_dpi:getText())
+	end
+
+	if not scale then
+		self:setLabel("Bad scale.")
+
+	elseif not dpi then
+		self:setLabel("Bad DPI.")
+
+	else
+		-- A dirty hack to prevent attempting (and failing) to load non-existent sets of textures.
+		-- TODO: Probably need to declare valid DPI numbers somewhere.
+		local tex_dir = love.filesystem.getInfo(context.conf.prod_ui_path .. "resources/textures/" .. tostring(dpi), "directory")
+		if not tex_dir then
+			btn:setLabel("Unprovisioned DPI")
+		else
+			context:setScale(scale)
+			context:setDPI(dpi)
+
+			if not (scale == old_scale and dpi == old_dpi) then
+				local theme = demoShared.loadTheme()
+
+				context.root:forEach(function(self) if self.skinner then self:skinRemove() end end)
+				context:applyTheme(theme)
+				context.root:forEach(function(self) if self.skinner then self:skinSetRefs(); self:skinInstall() end end)
+				context.root:reshape()
+			end
+			btn:setLabel("Update")
+		end
+	end
+end
+
+
 function plan.make(panel)
 	panel:setLayoutBase("viewport-width")
 	panel:setScrollRangeMode("auto")
@@ -11,7 +59,7 @@ function plan.make(panel)
 
 	demoShared.makeTitle(panel, nil, "Scaling")
 
-	demoShared.makeParagraph(panel, nil, "***Under construction*** --> This feature doesn't work yet.")
+	demoShared.makeParagraph(panel, nil, "***Work in Progress***")
 
 	local input
 
@@ -23,10 +71,7 @@ function plan.make(panel)
 	input:initialize()
 	input:setTag("in_scale")
 	input:setText(tostring(panel.context.scale))
-
-	input.wid_action = function(self)
-		-- …
-	end
+	input.wid_action = _updateScale
 
 	yy = yy + hh + h_pad
 
@@ -36,10 +81,7 @@ function plan.make(panel)
 	input:initialize()
 	input:setTag("in_dpi")
 	input:setText(tostring(panel.context.dpi))
-
-	input.wid_action = function(self)
-		-- …
-	end
+	input.wid_action = _updateScale
 
 	yy = yy + hh + h_pad
 
@@ -50,49 +92,7 @@ function plan.make(panel)
 	btn:initialize()
 	btn.tag = "btn_crt"
 	btn:setLabel("Update")
-	btn.wid_buttonAction = function(self)
-		local context = self.context
-
-		local old_scale, old_dpi = context:getScale(), context:getDPI()
-		local scale, dpi
-
-		local in_scale = self.parent:findTag("in_scale")
-		if in_scale then
-			scale = tonumber(in_scale:getText())
-		end
-		local in_dpi = self.parent:findTag("in_dpi")
-		if in_dpi then
-			dpi = tonumber(in_dpi:getText())
-		end
-
-		if not scale then
-			self:setLabel("Bad scale.")
-
-		elseif not dpi then
-			self:setLabel("Bad DPI.")
-
-		else
-			-- A dirty hack to prevent attempting (and failing) to load non-existent sets of textures.
-			-- TODO: Probably need to declare valid DPI numbers somewhere.
-			local tex_dir = love.filesystem.getInfo(context.conf.prod_ui_path .. "resources/textures/" .. tostring(dpi), "directory")
-			if not tex_dir then
-				btn:setLabel("Unprovisioned DPI")
-			else
-				context:setScale(scale)
-				context:setDPI(dpi)
-
-				if not (scale == old_scale and dpi == old_dpi) then
-					local theme = demoShared.loadTheme()
-
-					context.root:forEach(function(self) if self.skinner then self:skinRemove() end end)
-					context:applyTheme(theme)
-					context.root:forEach(function(self) if self.skinner then self:skinSetRefs(); self:skinInstall() end end)
-					context.root:reshape()
-				end
-				btn:setLabel("Update")
-			end
-		end
-	end
+	btn.wid_buttonAction = _updateScale
 
 	yy = yy + hh + h_pad
 end
