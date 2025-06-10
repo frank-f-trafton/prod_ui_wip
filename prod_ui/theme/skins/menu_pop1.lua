28babb6c4e941dcbdd46ea2cb6758db14e351bfa
RBTS <frank_f_trafton@yahoo.com>
2025-06-10 20:01:47 -0300

    MenuPop scaling work
    Also, stuffing some static measurements into the widget skin.

--- prod_ui/theme/skins/menu_pop1.lua
+++ prod_ui/theme/skins/menu_pop1.lua
@@ -3,8 +3,6 @@ return {
 
 	box = "*boxes/panel",
 
-	separator_size = 1,
-
 	font_item = "*fonts/p",
 
 	slc_body = "*slices/atlas/menu_pop_body",
@@ -17,6 +15,31 @@ return {
 	tq_radio_on = "*quads/atlas/menu_radio_on",
 	tq_radio_off = "*quads/atlas/menu_radio_off",
 
+	separator_size = 1,
+	underline_width = 1,
+
+	pad_bijou_x1 = 2,
+	pad_bijou_x2 = 2,
+	pad_bijou_y1 = 2,
+	pad_bijou_y2 = 2,
+
+	bijou_draw_w = 24,
+	bijou_draw_h = 24,
+
+	pad_text_x1 = 4,
+	pad_text_x2 = 4,
+	pad_text_y1 = 4,
+	pad_text_y2 = 4,
+
+	pad_separator_y = 4,
+
+	pad_arrow_x1 = 4,
+	pad_arrow_x2 = 0,
+	arrow_draw_w = 24,
+	arrow_draw_h = 24,
+
+	pad_shortcut_x1 = 16,
+	pad_shortcut_x2 = 8,
 
 	color_separator = {0.125, 0.125, 0.125, 1.0},
 	color_select_glow = {1.0, 1.0, 1.0, 0.33},
