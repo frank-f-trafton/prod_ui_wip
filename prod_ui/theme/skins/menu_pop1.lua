3665423872d006020170e73551020889473af3fb
RBTS <frank_f_trafton@yahoo.com>
2025-06-11 16:16:50 -0300

    MenuPop: refactor to better support scaling

--- prod_ui/theme/skins/menu_pop1.lua
+++ prod_ui/theme/skins/menu_pop1.lua
@@ -15,31 +15,32 @@ return {
 	tq_radio_on = "*quads/atlas/menu_radio_on",
 	tq_radio_off = "*quads/atlas/menu_radio_off",
 
-	separator_size = 1,
+	separator_item_height = 4,
+	separator_graphic_height = 1,
+
 	underline_width = 1,
 
-	pad_bijou_x1 = 2,
-	pad_bijou_x2 = 2,
+	pad_x1 = 0,
+	pad_x2 = 18,
+
+	pad_bijou_x1 = 0,
+	pad_bijou_x2 = 0,
 	pad_bijou_y1 = 2,
 	pad_bijou_y2 = 2,
 
 	bijou_draw_w = 24,
 	bijou_draw_h = 24,
 
-	pad_text_x1 = 4,
-	pad_text_x2 = 4,
+	pad_text_x1 = 6,
+	pad_text_x2 = 0,
 	pad_text_y1 = 4,
 	pad_text_y2 = 4,
 
-	pad_separator_y = 4,
-
-	pad_arrow_x1 = 4,
-	pad_arrow_x2 = 0,
 	arrow_draw_w = 24,
 	arrow_draw_h = 24,
 
 	pad_shortcut_x1 = 16,
-	pad_shortcut_x2 = 8,
+	pad_shortcut_x2 = 0,
 
 	color_separator = {0.125, 0.125, 0.125, 1.0},
 	color_select_glow = {1.0, 1.0, 1.0, 0.33},
