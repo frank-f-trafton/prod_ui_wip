2f5903a4fe56d8f373927513fc7334a4d42b83d6
RBTS <frank_f_trafton@yahoo.com>
2026-01-29 22:30:39 -0400

    More decorations for Group widgets

--- prod_ui/widgets/base/separator.lua
+++ prod_ui/widgets/base/separator.lua
@@ -3,13 +3,21 @@ local context = select(1, ...)
 
 local uiAssert = require(context.conf.prod_ui_req .. "ui_assert")
 local uiGraphics = require(context.conf.prod_ui_req .. "ui_graphics")
+local uiSchema = require(context.conf.prod_ui_req .. "ui_schema")
 local uiTable = require(context.conf.prod_ui_req .. "ui_table")
 local uiTheme = require(context.conf.prod_ui_req .. "ui_theme")
+local wcPipe = context:getLua("shared/wc/wc_pipe")
+
+
+local pipe_styles = context.resources.pipe_styles
 
 
 local def = {
 	skin_id = "separator1",
 }
+
+
+wcPipe.attachMethods(def)
 
 
 local _nm_axis2d = uiTheme.named_maps.axis_2d
@@ -29,35 +37,9 @@ function def:getAxis()
 end
 
 
-function def:setPipeStyle(pipe_id)
-	uiAssert.typeEval(1, pipe_id, "string")
-
-	if pipe_id then
-		local p_st = self.skin.pipe_styles[pipe_id]
-		if not p_st then
-			error("unprovisioned PipeStyle: " .. pipe_id)
-		end
-
-		self.pipe_id = pipe_id
-	else
-		self.pipe_id = false
-	end
-
-	self:reshape()
-
-	return self
-end
-
-
-function def:getPipeStyle()
-	return self.pipe_id
-end
-
-
 function def:evt_initialize()
 	self.visible = true
 
-	self.pipe_id = false
 	self.axis = "x"
 
 	-- Updated during reshape:
@@ -67,12 +49,14 @@ function def:evt_initialize()
 
 	self:skinSetRefs()
 	self:skinInstall()
+
+	self.PIPE_id = self.skin.default_pipe_id
 end
 
 
 function def:evt_reshapePre()
 	local skin = self.skin
-	local p_st = skin.pipe_styles[self.pipe_id]
+	local p_st = pipe_styles[self.PIPE_id]
 
 	if p_st then
 		if self.axis == "x" then
@@ -88,9 +72,19 @@ function def:evt_reshapePre()
 end
 
 
+local themeAssert = context:getLua("core/res/theme_assert")
+
+
 def.default_skinner = {
-	--validate = uiSchema.newKeysX {} -- TODO
-	--transform = function(scale, skin) -- TODO
+	validate = uiSchema.newKeysX {
+		skinner_id = {uiAssert.type, "string"},
+
+		default_pipe_id = themeAssert.pipeStyleID,
+		pipe_color = uiAssert.loveColorTuple
+	},
+
+
+	--transform = function(scale, skin)
 
 
 	install = function(self, skinner, skin)
@@ -108,10 +102,10 @@ def.default_skinner = {
 
 
 	render = function(self, ox, oy)
-		local pipe_id = self.pipe_id
+		local pipe_id = self.PIPE_id
 		if pipe_id then
 			local skin = self.skin
-			local p_st = skin.pipe_styles[pipe_id]
+			local p_st = pipe_styles[pipe_id]
 			if not p_st then
 				error("missing or invalid PipeStyle: " .. tostring(pipe_id))
 			end
